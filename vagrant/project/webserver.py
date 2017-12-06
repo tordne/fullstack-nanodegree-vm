627bc1e35f89f57da21234b707a6f041f426c47a
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2017-12-06 00:04:31 +0000

    Feat: Add /Hola as a path

--- vagrant/project/webserver.py
+++ vagrant/project/webserver.py
@@ -1,33 +1,47 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
 class webserverHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		try:
-			if self.path.endswith("/hello"):
-				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
-				self.end_headers()
 
-				output = "".encode()
-				output += "<html><body>Hello!</body></html>".encode()
-				self.wfile.write(output)
-				print(output)
-				return
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
 
-		except IOError:
-			self.send_error(404, "File Not Found {}".format(self.path))
+                output = "".encode()
+                output += "<html><body>Hello!</body></html>".encode()
+                self.wfile.write(output)
+                print(output)
+                return
+
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = "".encode()
+                output += "<html><body>&#161Hola <a href = '/hello'>Back to Hello \
+                </a></body></html>".encode()
+                self.wfile.write(output)
+                print(output)
+                return
+
+        except IOError:
+            self.send_error(404, "File Not Found {}".format(self.path))
 
 
 def main():
-	try:
-		port = 8080
-		server = HTTPServer(('',port), webserverHandler)
-		print("Web server running on port {}".format(port))
-		server.serve_forever()
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print("Web server running on port {}".format(port))
+        server.serve_forever()
 
-	except KeyboardInterrupt:
-		print("^C entered, stopping web server...")
-		server.socket.close()
+    except KeyboardInterrupt:
+        print("^C entered, stopping web server...")
+        server.socket.close()
 
 if __name__ == '__main__':
-	main()
+    main()
